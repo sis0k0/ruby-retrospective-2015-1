6d5caa97ddaba5c1331e2e60c1d7c90dbd06070b
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2016-01-16 12:32:58 +0200

    Refactors position_ahead method
    Get_next_position is now called position_ahead, following Ruby
    conventions. Makes use of multiple (destructive) assignment.

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,23 +1,15 @@
-def get_next_position(snake, direction)
-  head = snake.last
-  [head[0] + direction[0], head[1] + direction[1]]
-end
-
-def in_bounds?(position, dimensions)
-  width = dimensions[:width]
-  height = dimensions[:height]
-  x = position[0]
-  y = position[1]
-
-  x.between?(0, width - 1) and y.between?(0, height - 1)
+def position_ahead(snake, direction)
+  head_x, head_y = snake.last
+  direction_x, direction_y = direction
+  [head_x + direction_x, head_y + direction_y]
 end
 
 def grow(snake, direction)
-  snake.dup.push(get_next_position(snake, direction))
+  snake.dup.push(position_ahead(snake, direction))
 end
 
 def move(snake, direction)
-  snake.drop(1).push(get_next_position(snake, direction))
+  snake.drop(1).push(position_ahead(snake, direction))
 end
 
 def new_food(food, snake, dimensions)
@@ -25,21 +17,17 @@ def new_food(food, snake, dimensions)
 end
 
 def obstacle_ahead?(snake, direction, dimensions)
-  next_position = get_next_position(snake, direction)
-  not in_bounds?(next_position, dimensions) or snake.include?(next_position)
+  next_position = position_ahead(snake, direction)
+  not in_bounds?(next_position, dimensions) or body_ahead?(snake, next_position)
 end
 
 def danger?(snake, direction, dimensions)
-  obstacle_in_one_turn = obstacle_ahead?(snake, direction, dimensions)
-
   next_position = move(snake, direction)
+
+  obstacle_in_one_turn = obstacle_ahead?(snake, direction, dimensions)
   obstacle_in_two_turns = obstacle_ahead?(next_position, direction, dimensions)
 
   obstacle_in_one_turn or obstacle_in_two_turns
-end
-
-def position_taken?(food, snake, position)
-  snake.include?(position) or food.include?(position)
 end
 
 def available_positions(food, snake, dimensions)
@@ -51,4 +39,16 @@ def playground(dimensions)
   x_positions = (0...dimensions[:width]).to_a
   y_positions = (0...dimensions[:height]).to_a
   x_positions.product y_positions
+end
+
+def in_bounds?(position, dimensions)
+  width = dimensions[:width]
+  height = dimensions[:height]
+  x, y = position
+
+  x.between?(0, width - 1) and y.between?(0, height - 1)
+end
+
+def body_ahead?(snake, position)
+  snake.include?(position)
 end
