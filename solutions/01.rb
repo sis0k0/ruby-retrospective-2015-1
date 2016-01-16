86b9c9776e2c84c43f5c5161ff86b7eda048663f
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2016-01-16 11:21:49 +0200

    Makes variables' names more understandable

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,19 +1,20 @@
-CURRENCY_TO_BGN = {
+BGN_EXCHANGE_RATES = {
+  bgn: 1,
   usd: 1.7408,
   eur: 1.9557,
   gbp: 2.6415,
-  bgn: 1,
 }
 
 PRECISION = 2
 
 def convert_to_bgn(price, currency)
-  (price * CURRENCY_TO_BGN[currency]).round PRECISION
+  price_bgn = price * BGN_EXCHANGE_RATES[currency]
+  price_bgn.round PRECISION
 end
 
 def compare_prices(first_price, first_currency, second_price, second_currency)
-  first_bgn = convert_to_bgn(first_price, first_currency)
-  second_bgn = convert_to_bgn(second_price, second_currency)
+  first_price_bgn = convert_to_bgn(first_price, first_currency)
+  second_price_bgn = convert_to_bgn(second_price, second_currency)
 
-  first_bgn <=> second_bgn
+  first_price_bgn <=> second_price_bgn
 end
