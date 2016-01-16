4b79d6ed987b9f9db152963506cb6f901efc99de
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2016-01-16 12:15:59 +0200

    Remakes the in_bounds? method
    Makes use of the between? method.

--- solutions/02.rb
+++ solutions/02.rb
@@ -3,13 +3,13 @@ def get_next_position(snake, direction)
   [head[0] + direction[0], head[1] + direction[1]]
 end
 
-def out_of_bounds?(position, dimensions)
+def in_bounds?(position, dimensions)
   width = dimensions[:width]
   height = dimensions[:height]
   x = position[0]
   y = position[1]
 
-  x < 0 or x >= width or y < 0 or y >= height
+  x.between?(0, width - 1) and y.between?(0, height - 1)
 end
 
 def grow(snake, direction)
@@ -26,18 +26,16 @@ end
 
 def obstacle_ahead?(snake, direction, dimensions)
   next_position = get_next_position(snake, direction)
-  out_of_bounds = out_of_bounds?(next_position, dimensions)
-
-  out_of_bounds or snake.include?(next_position)
+  not in_bounds?(next_position, dimensions) or snake.include?(next_position)
 end
 
 def danger?(snake, direction, dimensions)
-  obstacle_one_turn = obstacle_ahead?(snake, direction, dimensions)
+  obstacle_in_one_turn = obstacle_ahead?(snake, direction, dimensions)
 
   next_position = move(snake, direction)
-  obstacle_two_turns = obstacle_ahead?(next_position, direction, dimensions)
+  obstacle_in_two_turns = obstacle_ahead?(next_position, direction, dimensions)
 
-  obstacle_one_turn or obstacle_two_turns
+  obstacle_in_one_turn or obstacle_in_two_turns
 end
 
 def position_taken?(food, snake, position)
